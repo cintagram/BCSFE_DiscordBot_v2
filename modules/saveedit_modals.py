91a5652c22e869f4beef1ae88e769c451a689edb
CatKnife <72387902+cintagram@users.noreply.github.com>
2024-03-30 10:10:43 +0900

    Update saveedit_modals.py

--- modules/saveedit_modals.py
+++ modules/saveedit_modals.py
@@ -1,30 +1,26 @@
+import discord
+from discord.ext import commands
+from discord.ui import Button, TextInput
+from discord import app_commands,Interaction,ui,ButtonStyle,SelectOption
+import json
+import datetime
+import os
+import sys
+import BCSFE_Python_Discord as BCSFE_Python
+from BCSFE_Python_Discord import *
 
-
-
-class TCInputModal(ui.Modal):
-            tccode = ui.TextInput(
-                  label="기종변경 코드",
-                  style=discord.TextStyle.short,
-                  placeholder="기종변경코드 입력",
-                  default=""
-            )
-            cccode = ui.TextInput(
-                  label="인증번호",
-                  style=discord.TextStyle.short,
-                  placeholder="인증번호 입력",
-                  default=""
-            )
-            gv_input = ui.TextInput(
-                  label="게임버전",
-                  style=discord.TextStyle.short,
-                  placeholder="게임버전 | 예시: 13.1.1",
-                  default=""
-            )
-            
-            def __init__(self, country):
-                  self.country = country
-                  super().__init__(title="계정정보 입력")
-            
+class ItemInputModal_Single(ui.Modal):
+            def __init__(self, itemname, currentvalue, maxvalue):
+				self.itemname = itemname
+				self.cv = currentvalue
+				self.mv = maxvalue
+				super().__init__(title="값 입력")
+				self.inputvalue = ui.TextInput(
+					label=f"{self.itemname} 값 입력",
+					style=discord.TextStyle.short,
+					placeholder=f"현재값: {self.cv} | 최댓값: {self.mv}",
+					default=""
+				)
             async def mainmenu_cb(self, interaction: Interaction):
                   if self.mainselect.values[0] == "save_management":
                         interaction.response.edit_message(content="save_management menu")
@@ -33,7 +29,7 @@ class TCInputModal(ui.Modal):
                   async def mainmenu_cb(self, interaction: Interaction):
                         if self.mainselect.values[0] == "save_management":
                               interaction.response.edit_message(content="save_management menu")
-                  await interaction.response.send_message(content="DM을 확인해주세요.", ephemeral=True)
+                  #await interaction.response.send_message(content="DM을 확인해주세요.", ephemeral=True)
                   processingmsg = await interaction.user.send("세이브 데이터를 받아오는 중입니다.\n잠시만 기다려주세요.")
                   await processingmsg.delete()
                   userid = str(interaction.user.id)
