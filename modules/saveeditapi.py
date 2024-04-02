6ced215b0a130b3b698e2b87fea247021629e7cb
CatKnife <72387902+cintagram@users.noreply.github.com>
2024-04-02 17:29:08 +0900

    Update saveeditapi.py

--- modules/saveeditapi.py
+++ modules/saveeditapi.py
@@ -9,6 +9,9 @@ import sys
 import BCSFE_Python_Discord as BCSFE_Python
 from BCSFE_Python_Discord import *
 
-def CatFoodEdit(save_stats, value):
+def srvmemberpath(srvid: str, memid: str):
+      return os.path.abspath(os.path.join(os.path.join(os.path.join(os.path.join(os.path.curdir, "bc_saves"), "servers"), srvid), memid))
+
+def CatFoodEdit(save_stats, value, userid, srvid):
 	save_stats["cat_food"]["Value"] = int(value)
-	return save_stats
+	open(os.path.join(srvmemberpath(str(srvid), str(userid)), "save_json_temp.json"), "w+", encoding="utf-8").write(json.dumps(save_stats))
