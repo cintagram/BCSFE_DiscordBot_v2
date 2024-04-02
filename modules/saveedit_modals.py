b8e07c4f0d271f8cee32b77d4a125601759aeafa
CatKnife <72387902+cintagram@users.noreply.github.com>
2024-04-02 17:31:16 +0900

    Update saveedit_modals.py

--- modules/saveedit_modals.py
+++ modules/saveedit_modals.py
@@ -12,14 +12,15 @@ from BCSFE_Python_Discord import *
 
 class ItemInputModal_Single(ui.Modal):
     inputvalue = ui.TextInput(label=f"{itemname} 값 입력", style=discord.TextStyle.short, placeholder=f"현재값: {self.cv['cat_food']['Value']} | 최댓값: {self.mv}", default="")
-    def __init__(self, itemname, currentvalue, maxvalue, save_stats):
+    def __init__(self, itemname, currentvalue, maxvalue, save_stats, srvid):
         self.itemname = itemname
         self.cv = currentvalue
         self.mv = maxvalue
         self.sv = save_stats
+		self.srvid = int(srvid)
         super().__init__(title="값 입력")
 
 	async def on_submit(self, interaction: Interaction):
-		self.sv = saveeditapi.CatFoodEdit(self.sv, self.inputvalue.value, interaction.user.id)
+		self.sv = saveeditapi.CatFoodEdit(self.sv, self.inputvalue.value, interaction.user.id, self.srvid)
 		await interaction.user.send(content=f"Set CF to {self.inputvalue.value}")
 		return self.sv
